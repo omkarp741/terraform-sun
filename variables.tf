548e4f07bf6760971fc197bc16c7da9d0406d19d
omkarp741 <121652354+omkarp741@users.noreply.github.com>
2024-03-17 15:59:45 +0530

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,62 @@
+variable "region" {
+  
+}
+
+variable "access_key" {
+  
+}
+
+variable "secret_key" {
+  
+}
+
+variable "ami" {
+  
+}
+
+variable "instance_type" {
+
+} 
+
+variable "cidr_block" {
+  
+}
+
+variable "public1-cidr_block" {
+  
+}
+
+variable "public1-az" {
+  
+}
+
+variable "private2-cidr_block" {
+  
+}
+
+variable "private2-az" {
+  
+}
+
+variable "bucket" {
+  
+}
+
+variable "engine" {
+  
+}
+variable "instance_class" {
+  
+}
+
+variable "username" {
+  
+}
+
+variable "password" {
+  
+}
+
+
+
+
